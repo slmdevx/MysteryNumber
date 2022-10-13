f219ede5d7704ea2cf522b6499faef4dc0dd4ed7
Sergio Machado <sergiolmachado@gmail.com>
2022-10-13 19:04:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿int guessNum = ChooseNumber();
+int generatedNumber = GenerateRandomNumber();
+int numOfIterations = 0;
+
+while (guessNum != generatedNumber)
+{
+    Console.WriteLine($"Numero gerado: {generatedNumber}");
+    Console.WriteLine($"Numero escolhido: {guessNum}");
+
+    if(generatedNumber > guessNum)
+    {
+        Console.WriteLine($"Mystery number is Higher than {guessNum}");
+    }
+    else if(generatedNumber < guessNum)
+    {
+        Console.WriteLine($"Mystery number is lower than {guessNum}");
+    }
+
+    numOfIterations++;
+    guessNum = ChooseNumber();
+}
+
+Console.WriteLine($"That is correct. You guessed the number after {numOfIterations} times!");
+Console.ReadLine();
+
+static int GenerateRandomNumber()
+{
+    Random randomNum = new();
+    var generatedNumber = randomNum.Next(1, 5);
+    return generatedNumber;
+}
+
+static int ChooseNumber()
+{
+    int guessNum;
+
+    Console.WriteLine("Choose a number between 1 and 5");
+    string? strGuess = Console.ReadLine();
+
+    while (int.TryParse(strGuess, out guessNum) == false || guessNum < 1 || guessNum > 100)
+    {
+        Console.WriteLine("Invalid value");
+    };
+    return guessNum;
+}
